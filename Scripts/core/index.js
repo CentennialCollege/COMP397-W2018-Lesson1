af0acf4814410b9885395fe7ba8611a3af1d48ba
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-01-27 15:01:11 -0500

    added typescript support

--- Scripts/core/index.js
+++ Scripts/core/index.js
@@ -1,23 +1,14 @@
 // IIFE -- Immediately Invoked Function Expression
-(function(){
-
-function Init() {
-  console.log("Game Started");
-}
-
-function Start() {
-
-}
-
-function Update() {
-
-}
-
-function Main() {
-
-}
-
-window.onload = Init;
-
-
+(function () {
+    function Init() {
+        console.log("Game Started");
+    }
+    function Start() {
+    }
+    function Update() {
+    }
+    function Main() {
+    }
+    window.onload = Init;
 })();
+//# sourceMappingURL=index.js.map
---
+++ Scripts/core/index.ts
@@ -0,0 +1,23 @@
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+function Init():void {
+  console.log("Game Started");
+}
+
+function Start():void {
+
+}
+
+function Update():void {
+
+}
+
+function Main():void {
+
+}
+
+window.onload = Init;
+
+
+})();

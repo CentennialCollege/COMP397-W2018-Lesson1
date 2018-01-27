8a31c2eac83525d3c5ad288ffbef79a264b5558c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-01-27 15:26:13 -0500

    added createjs support

--- Scripts/core/index.js
+++ Scripts/core/index.js
@@ -1,13 +1,29 @@
 // IIFE -- Immediately Invoked Function Expression
 (function () {
+    // Game Variables
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
     function Init() {
-        console.log("Game Started");
+        console.log("Initialization Started");
+        Start();
     }
     function Start() {
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; // 60 fps
+        createjs.Ticker.on("tick", Update); // call update every frame
+        Main();
     }
     function Update() {
+        helloLabel.rotation += 5;
+        stage.update();
     }
     function Main() {
+        console.log("Game Started...");
+        helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel);
     }
     window.onload = Init;
 })();
--- Scripts/core/index.ts
+++ Scripts/core/index.ts
@@ -1,20 +1,37 @@
 // IIFE -- Immediately Invoked Function Expression
 (function(){
 
+  // Game Variables
+  let canvas = document.getElementById("canvas");
+  let stage: createjs.Stage;
+  let helloLabel: createjs.Text;
+
+
 function Init():void {
-  console.log("Game Started");
+  console.log("Initialization Started");
+  Start();
 }
 
 function Start():void {
-
+  stage = new createjs.Stage(canvas);
+  createjs.Ticker.framerate = 60; // 60 fps
+  createjs.Ticker.on("tick", Update); // call update every frame
+  Main();
 }
 
 function Update():void {
-
+  helloLabel.rotation += 5;
+  stage.update();
 }
 
 function Main():void {
+  console.log("Game Started...");
 
+  helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+  helloLabel.x = 100;
+  helloLabel.y = 100;
+
+  stage.addChild(helloLabel);
 }
 
 window.onload = Init;

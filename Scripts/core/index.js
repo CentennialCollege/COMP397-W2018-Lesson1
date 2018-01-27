5cc6efad23ffdae107e1504f8fac7774aa093bb1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-01-27 14:46:21 -0500

    initial commit

---
+++ Scripts/core/index.js
@@ -0,0 +1,23 @@
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+function Init() {
+  console.log("Game Started");
+}
+
+function Start() {
+
+}
+
+function Update() {
+
+}
+
+function Main() {
+
+}
+
+window.onload = Init;
+
+
+})();
